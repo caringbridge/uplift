f2fc83d1b509bd4a9614e2401357dfb3e7bf108c
Stephen Van Dahm <code@vandahm.com>
2012-10-15 14:20:44 -0500

    Momentary checkin

--- index.php
+++ index.php
@@ -4,11 +4,21 @@
     <div class="row">
         <div class="span8">
             <div id="uplift-header" class="row">
-                <div class="span5">
+                <div class="span4 uplift-logo-container">
                     <img src="<?php bloginfo('template_directory'); ?>/img/uplift-logo.svg" alt="Uplift Logo">
                 </div>
-                <div class="span3">
-                    social widgets
+                <div class="span4 widgets">
+                    <span>
+                    <strong class="visible-desktop">Share our blog:</strong>
+                    </span>
+                    
+                    <a class="facebook" href="http://www.facebook.com/caringbridge" onclick="window.open(this.href,'facebook','menubar=yes,scrollbars=yes,resizable=yes,width=800,height=700,left=100,top=75'); stopDefaultAction(event);" data-ga-label="Facebook" data-ga-action="Click - image" title="Become a fan on Facebook"><span>Become a fan on Facebook</span></a>
+                    
+                    <a class="twitter" href="http://www.twitter.com/caringbridge" onclick="window.open(this.href,'help','menubar=yes,scrollbars=yes,resizable=yes,width=800,height=700,left=100,top=75'); stopDefaultAction(event);" data-ga-label="Twitter" data-ga-action="Click - image" title="Follow @CaringBridge on Twitter"><span>Follow @CaringBridge on Twitter</span></a>
+                    
+                    <a class="googleplus" href="https://plus.google.com/113554774990455135149" onclick="window.open(this.href,'help','menubar=yes,scrollbars=yes,resizable=yes,width=800,height=700,left=100,top=75'); stopDefaultAction(event);" data-ga-label="Google+" data-ga-action="Click - image" title="Follow CaringBridge on Google+"><span>Follow CaringBridge on Google+</span></a>
+                    
+                    <a class="youtube" href="http://www.youtube.com/caringbridge" onclick="window.open(this.href,'help','menubar=yes,scrollbars=yes,resizable=yes,width=800,height=700,left=100,top=75'); stopDefaultAction(event);" data-ga-label="YouTube" data-ga-action="Click - image" title="Find us on YouTube"><span>Find us on YouTube</span></a>
                 </div>
             </div>
             <?php if ( have_posts() ): ?>
@@ -18,10 +28,13 @@
                         <h1>
                             <a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a>
                         </h1>
-            			<!-- <time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?> -->
+                        <p class="byline">
+                        by <?php the_author_posts_link(); ?> &#183; <time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date('F j, Y'); ?></time> &#183; Find plugin for views
+                        </p>
+
                         <div class="row">
                             <?php if (has_post_thumbnail()): ?>
-                    			<div class="span2">
+                                <div class="span2 hidden-phone thumbnail-container">
                                     <?php the_post_thumbnail('thumbnail'); ?>
                                 </div>
                     			<div class="span6">
@@ -45,7 +58,7 @@
             <h2>No posts to display</h2>
             <?php endif; ?>
         </div>
-        <div class="span3 offset1" style="background:#ff0;">
+        <div class="span3 offset1">
             <?php get_sidebar(); ?>
         </div>
     </div> <!-- div.row -->
